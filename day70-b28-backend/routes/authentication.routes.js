7b05a91be738e5d359329b14ef3c74805cdfab11
LakshmiNarasimhan273 <bachelorofphotography@gmail.com>
2026-06-30 11:53:57 +0530

    auth: Register & Login

--- day70-b28-backend/routes/authentication.routes.js
+++ day70-b28-backend/routes/authentication.routes.js
@@ -6,4 +6,6 @@ const authentication = require("../security/authentication.security");
 // http://localhost:8080/auth/register
 router.post("/register", authentication.registerUser);
 
+router.post("/login", authentication.loginUser);
+
 module.exports = router;
--- day70-b28-backend/security/authentication.security.js
+++ day70-b28-backend/security/authentication.security.js
@@ -1,6 +1,7 @@
 const userModel = require("../model/user.model");
 
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // registerApi
 // method: POST,
@@ -11,9 +12,9 @@ const registerUser = async (req, res) => {
 
         // logic 1: email check
         const emailCheck = await userModel.findOne({ email });
-
-        if(emailCheck){
-            return res.status(400).json({message: "User account already exists"});
+        // boolean: true{email is exists in our db} (or) false{email does'nt exists in our db}
+        if (emailCheck) {
+            return res.status(400).json({ message: "User account already exists" });
         }
 
         // logic 2: password hashing
@@ -28,4 +29,44 @@ const registerUser = async (req, res) => {
     }
 };
 
-module.exports = { registerUser };
+// loginApi
+// method: POST,
+// access: Public
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email });
+
+        if(!user){
+            return res.status(404).json({ message: "User account not found" });
+        }
+
+        // crucial: step
+        // compare(dynamic function) - body(password) - hash
+        // compare - body hash === db hash
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if(!passwordMatch){
+            return res.status(401).json({message: "Invalid Password"});
+        }
+
+        // server creates a jwt token
+        // sign function holds 3 parameters
+        // 1 user details to encrypt in the token
+        // 2 secret key
+        // 3 expiry time
+        const token = jwt.sign(
+            {userId: user._id, username: user.username, email: user.email},
+            process.env.jwt_secret,
+            {expiresIn: '24h'}
+        );
+
+        res.status(200).json({message: "Login Successful", token});
+
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+}
+
+module.exports = { registerUser, loginUser };
